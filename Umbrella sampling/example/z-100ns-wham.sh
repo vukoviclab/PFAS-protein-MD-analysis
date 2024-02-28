385111523a57053e35741d6adf801c182b3f4b3a
Anju Yadav <141439226+anju-yaadav@users.noreply.github.com>
2024-02-28 08:56:17 +0530

    Add files via upload

---
+++ Umbrella sampling/example/z-100ns-wham.sh
@@ -0,0 +1,3 @@
+#wham [P|Ppi|Pval] hist_min hist_max num_bins tol temperature numpad metadatafile freefile [num_MC_trials randSeed] 
+#
+/path-to-wham/wham  0 7 100 0.01 310  0  metadatafile_100ns  freefile_100ns  50 123 
